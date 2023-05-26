dc96552c38550a8b35660d0507a844f33f83589b
yoojunsu <vcoavmv@gmail.com>
2023-05-26 15:23:14 +0900

    [포폴갤러리] '메인-어바웃' 영역 작업 완료
    1. 메인 어바웃 영역 퍼블리싱 작업 완료
    2. 프로필 이미지 추가
    3. 스킬 탭버튼 동작 이벤트 js 추가

--- main.php
+++ main.php
@@ -36,7 +36,219 @@
 <!-- ...main visual 섹션 -->
 
 <!-- about(어바웃) 섹션 -->
-<section class="about-section"></section>
+<section class="about-section">
+    <div class="section-inner">
+        <div class="section-tit-wrap">
+            <h2 class="section-tit">ABOUT</h2>
+        </div>
+        
+        <!-- about 상단 컨텐츠 -->
+        <div class="about-top-wrap">
+            <!-- about 프로필 이미지 -->
+            <div class="about-img-box border-ani">
+                <img src="./src/img/about-profile.png" alt="프로필 이미지" />
+            </div>
+            <!-- about 프로필 이미지 -->
+            
+            <!-- about 인사말 -->
+            <div class="about-greeting-box">
+                <h3 class="about-greeting-tit">
+                    Front-End Developer
+                </h3>
+                <p class="about-greeting-txt">
+                    안녕하세요. <br />
+                    프로가 되고자 하는 개발자. <br />
+                    유준수 입니다.
+                </p>
+            </div>
+            <!-- about 상단 컨텐츠 -->
+        </div>
+
+        <!-- about 메인 컨텐츠 -->
+        <div class="about-main-wrap">
+            <div class="about-my-box">
+                <!-- about 메인 - 프로필 컨텐츠 -->
+                <div class="about-my border-ani" id="aboutProfile">
+                    <div class="about-my-top">
+                        <div class="about-my-icon">
+                            <i class="fa-solid fa-address-card"></i>
+                        </div>
+                        <h4 class="about-my-tit">
+                            PROFILE
+                        </h4>
+                    </div>
+                    <div class="about-my-content">
+                        <ul class="about-my-profile-list">
+                            <li class="about-my-profile">
+                                <i class="fa-regular fa-square-check"></i>
+                                <span class="about-my-desc about-profile-subject">
+                                    Name:
+                                </span>
+                                <span class="about-my-desc">
+                                    유준수
+                                </span>
+                            </li>
+                            <li class="about-my-profile">
+                                <i class="fa-regular fa-square-check"></i>
+                                <span class="about-my-desc about-profile-subject">
+                                    Call:
+                                </span>
+                                <span class="about-my-desc">
+                                    010-8527-9631
+                                </span>
+                            </li>
+                            <li class="about-my-profile">
+                                <i class="fa-regular fa-square-check"></i>
+                                <span class="about-my-desc about-profile-subject">
+                                    Add:
+                                </span>
+                                <span class="about-my-desc">
+                                    경기도 의정부시 거주
+                                </span>
+                            </li>
+                            <li class="about-my-profile">
+                                <i class="fa-regular fa-square-check"></i>
+                                <span class="about-my-desc about-profile-subject">
+                                    Edu:
+                                </span>
+                                <span class="about-my-desc">
+                                    더조은 컴퓨터 아카데미
+                                    웹퍼블리셔 UI/UX 프론트엔드 양성과정 수료
+                                </span>
+                            </li>
+                            <li class="about-my-profile">
+                                <i class="fa-regular fa-square-check"></i>
+                                <span class="about-my-desc">
+                                    열심히 하는 <strong>'직원'</strong>이 아닌,
+                                    이름을 걸고 의미있는 결과를 만들어내는 <strong>'프로'</strong>가 되고자합니다.
+                                    트렌드를 따라가고 선도하기 위해 스스로 디벨롭하는 <strong>'프로'</strong>가 되겠습니다.
+                                </span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- ...about 메인 - 프로필 컨텐츠 -->
+                
+                <!-- about 메인 - 커리어(경력) 컨텐츠 -->
+                <div class="about-my border-ani" id="aboutCareer">
+                    <div class="about-my-top">
+                        <div class="about-my-icon">
+                            <i class="fa-solid fa-building"></i>
+                        </div>
+                        <h4 class="about-my-tit">
+                            CAREER
+                        </h4>
+                    </div>
+                    <div class="about-my-content">
+                        <ul class="about-my-career-list">
+                            <li class="about-my-content">
+                                <h4 class="about-my-career-name">
+                                    퍼플스 그룹
+                                </h4>
+
+                                <ul class="about-career-work-list">
+                                    <li class="about-my-career">
+                                        <span class="about-my-desc">
+                                            웹 개발팀 주임 (퍼블리셔)
+                                        </span>
+                                    </li>
+                                    <li class="about-my-career">
+                                        <span class="about-my-desc">
+                                            2021.02 ~ ing
+                                        </span>
+                                    </li>
+                                    <li class="about-my-career">
+                                        <span class="about-my-desc">
+                                            퍼플스 그룹 소속 업체<br />
+                                            웹&앱 퍼블리싱 및 유지보수 파트 담당
+                                        </span>
+                                    </li>
+                                </ul>
+                            </li>
+
+                            <li class="about-my-content">
+                                <h4 class="about-my-career-name">
+                                    전율 디자인 에이전시
+                                </h4>
+                                <ul class="about-career-work-list">
+                                    <li class="about-my-career">
+                                        <span class="about-my-desc">
+                                            웹 서비스팀 사원
+                                        </span>
+                                    </li>
+                                    <li class="about-my-career">
+                                        <span class="about-my-desc">
+                                            2020.08 ~ 2020. 11
+                                        </span>
+                                    </li>
+                                    <li class="about-my-career">
+                                        <span class="about-my-desc">
+                                            카페24 기반 템플릿 퍼블리싱 & 유지보수 & 클라이언트 응대
+                                        </span>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- ...about 메인 - 커리어(경력) 컨텐츠 -->
+
+                <!-- about 메인 - 스킬 컨텐츠 -->
+                <div class="about-my border-ani" id="aboutSkils">
+                    <div class="about-my-top">
+                        <div class="about-my-icon">
+                            <i class="fa-solid fa-file-code"></i>
+                        </div>
+                        <h4 class="about-my-tit">
+                            SKILLS
+                        </h4>
+                    </div>
+                    <div class="about-my-content">
+                        <div class="about-my-skil-wrap">
+                            <button type="button" class="about-my-skil-btn border-ani active">
+                                <i class="fa-brands fa-html5"></i>
+                                <b class="about-my-skil-name">HTML5</b>
+                            </button>
+                            <button type="button" class="about-my-skil-btn border-ani">
+                                <i class="fa-brands fa-css3-alt"></i>
+                                <b class="about-my-skil-name">Css3</b>
+                            </button>
+                            <button type="button" class="about-my-skil-btn border-ani">
+                                <i class="fa-brands fa-square-js"></i>
+                                <b class="about-my-skil-name">JavaScript</b>
+                            </button>
+                            <button type="button" class="about-my-skil-btn border-ani">
+                                <i class="fa-brands fa-vuejs"></i>
+                                <b class="about-my-skil-name">Vue.js</b>
+                            </button>
+                            <button type="button" class="about-my-skil-btn border-ani">
+                                <i class="fa-brands fa-sass"></i>
+                                <b class="about-my-skil-name">Scss</b>
+                            </button>
+                            <button type="button" class="about-my-skil-btn border-ani">
+                                <i class="fa-brands fa-git-alt"></i>
+                                <b class="about-my-skil-name">Git</b>
+                            </button>
+                        </div>
+
+                        <div class="about-my-skil-detail-box border-ani">
+                            <h4 class="about-my-skil-detail-name border-ani">
+                                HTML 5
+                            </h4>
+
+                            <p class="about-my-skil-detail-desc">
+                                시멘틱 요소를 활용하여 웹 페이지의 구조를 더 명확하게 표현하고, <br />
+                                목적에 맞는 디테일한 class명을 기입하여 추후 유지보수하는데 있어 용이하도록 마크업을 합니다.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <!-- ...about 메인 - 스킬 컨텐츠 -->
+            </div>
+        </div>
+        <!-- ...about 메인 컨텐츠 -->
+    </div>
+</section>
 <!-- ...about(어바웃) 섹션 -->
 
 <!-- work(프로젝트) 섹션 -->
--- src/js/common.js
+++ src/js/common.js
@@ -1,8 +1,69 @@
 window.addEventListener("DOMContentLoaded", () => {
     
+    //about skill 탭컨텐츠 실행
+    const SKILLS_DETAIL = [
+        {
+            "name": `HTML5`,
+            "description": `HTML5의 시멘틱 요소를 활용하여 웹 페이지의 구조를 더 명확하게 표현하고,
+            목적에 맞는 디테일한 class명을 기입하여 추후 유지보수하는데 있어 용이하도록 마크업을 합니다.`
+        },
+        {
+            "name": `CSS3`,
+            "description": `CSS3 기술인 flex, grid 기술을 활용하여 레이아웃을 구축 할 수 있으며,
+            IE 서비스 종료 이후 더욱 적극적으로 사용하여 스킬업을 하고 있습니다, 
+            또한 디자인 시안에 맞춰 웹 페이지,웹 애플리케이션등 을 구축 할 수 있습니다.`
+        },
+        {
+            "name": `JAVASCRIPT`,
+            "description": `Javscript의 기본문법을 이해하고 동적인 웹 페이지, 웹 애플리케이션을 구축 할 수 있습니다.
+            웹과 상호작용하여 이벤트 처리, 데이터 처리를 할 수 있으며, axios를 활용하여 서버와 비동기적 데이터를 주고 받을 수 있습니다.
+            추가적으로 DOM을 쉽고 빠르게 제어 할 수 있는 JS라이브러리 제이쿼리를 능숙하게 다룰 수 있으며, swiper등과 같은 ui를 구현해주는 플러그인을 능숙하게 다룰 수 있습니다. 
+            프레임워크(Vue.js)를 학습 & 공부를 지속적으로 하면서 효율적이고 강력한 웹 개발을 지향하고있습니다.`,
+        },
+        {
+            "name": `Vue.js`,
+            "description": `Vue.js를 활용하여 재사용이 가능한 컴포넌트 기반의 웹 애플리케이션을 구축 할 수 있습니다.
+            'v-for','v-if','데이터바인딩' 등 Vue의 기본 문법을 이해하고, state를 관리하는 'Vuex',컴포넌트간 데이터를 처리할때 사용하는 'emitter'등의 다양한 플러그인을 활용 할 수 있습니다.
+            상대적으로 React에 비해 코드의 자율성이 낮지만 그만큼 유지보수를 하거나, 협업하는데 있어 장벽이 낮다라고 생각하여 선택했습니다.
+            트랜드에 맞춰 Vue3를 활용한 SPA(Single Page Application) 개발 방식을 선호합니다. 
+            Vue의 개발환경을 설정해주는 도구인 Vue Cli를 활용하며 webpack 설정에 대한 고민을 최소화 합니다.
+            `
+        },
+        {
+            "name": `SCSS`,
+            "description": `CSS전처리기인 SCSS을 활용하여 자주 사용하는 스타일과 컬러,속성등을 변수,mixin으로 관리하며, 
+            상위 선택자의 반복을 피하고 편리하게 복잡한 구조를 작성 할 수 있습니다. 
+            기본 문법을 활용하여 반복된 클래스명을 관리 할 수 있습니다.
+            `
+        },
+        {
+            "name": `Git`,
+            "description": `협업하는데 있어 Git의 기본 사용법을 이해하고, 
+            branch의 구조등을 논의하여 협업관리자와 함께 코드 관리를 간편하게 하며, 
+            컨플릭트가 일어났을 때 대처 할 수 있습니다. Git을 사용하여 팀원들과 업무 방식, 커밋 방식등의 개선점을 제안하며
+            효율적인 업무방식을 발전시키기위해 노력합니다.`
+        }
+
+    ];
+    const SKILLS_BTNS = document.querySelectorAll(".about-my-skil");
+    const SKILL_DETAIL_NAME_EL = document.querySelector(".about-my-skil-detail-name");
+    const SKILL_DETAIL_DESC_EL = document.querySelector(".about-my-skil-detail-desc");
+    
+    for(let i = 0; i < SKILLS_BTNS.length; i++) {
+        SKILLS_BTNS[i].addEventListener("click", (e) => {
+            activeClass = "active";
+            SKILLS_BTNS.forEach( (skillsBtn) => {
+                skillsBtn.classList.remove(activeClass);
+            });
+            
+            e.currentTarget.classList.add(activeClass);
+            SKILL_DETAIL_NAME_EL.textContent = SKILLS_DETAIL[i].name;
+            SKILL_DETAIL_DESC_EL.textContent = SKILLS_DETAIL[i].description;
+        });
+    }
 });
 
-
+let activeClass;
 /**
  * 타이핑 효과 함수
  * @param {타이핑 요소의 아이디or클래스 기입} typingEl 
