8d0f348988725566b37ea97ecd1f27529f68d00a
yoojunsu <vcoavmv@naver.com>
2023-10-10 01:47:34 +0900

    [포폴리뉴얼] form 전송 완료 레이어 모달 요소 추가
    1. include를 위한 별도 php파일 생성
    2. 모달요소 스타일 관리를 위한 별도의 scss 파일 생성

---
+++ _include/form-send-layer.php
@@ -0,0 +1,16 @@
+<div class="modal-wrap">
+    <div class="modal" id="formSendModal">
+        <div class="modal-text-wrap">
+            <h2 class="modal-tit">COMPLETE</h2>
+            <p class="modal-desc">
+                성공적으로 전송되었습니다. <br />
+                감사합니다.
+            </p>
+        </div>
+        <div class="modal-btn-wrap">
+            <button type="button" id="modalCloseBtn">
+                <span>닫기</span>
+            </button>
+        </div>
+    </div>
+</div>
