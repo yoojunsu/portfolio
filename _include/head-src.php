821e4dbc7b0273ebaee4b8845d1780bdf61978da
yoojunsu <vcoavmv@naver.com>
2023-10-10 01:25:26 +0900

    [포폴리뉴얼] 필요 리소스 추가
    1. 파비콘 리소스 기입
    2. 스크롤 애니메이션 js 플러그인 AOS CDN추가
    3. 비동기 통신 js 라이브러리 AXIOS CDN 추가
    4. 필요 폰트 CDN 추가

--- _include/head-src.php
+++ _include/head-src.php
@@ -1,10 +1,31 @@
+<!-- favicon -->
+<link rel="apple-touch-icon" sizes="57x57" href="/src/img/favicon/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="/src/img/favicon/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="/src/img/favicon/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="/src/img/favicon/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/src/img/favicon/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="/src/img/favicon/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="/src/img/favicon/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/src/img/favicon/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/src/img/favicon/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="/src/img/favicon/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/src/img/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="/src/img/favicon/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/src/img/favicon/favicon-16x16.png">
+<link rel="manifest" href="/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
 <!-- font & icon -->
 <script src="https://kit.fontawesome.com/def4bdd2e0.js" crossorigin="anonymous"></script>
-<link href="https://fonts.googleapis.com/css2?family=Anton&family=Noto+Sans+KR:wght@300;400;500&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Anton&family=Yellowtail&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/xeicon@2.3.3/xeicon.min.css">
-<!-- library -->
+<!-- library & plugin -->
 <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bodymovin/5.5.9/lottie.min.js"></script>
 <script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"> 
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script> 
+<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 <!-- css -->
 <link rel="stylesheet" href="/src/css/common.css" />
